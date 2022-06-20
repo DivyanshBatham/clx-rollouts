1194c0b17ed1ab4c541e6ed379867248a9afd65a
bilalk1510 <mohammadbilal.khan@unacademy.com>
2022-06-20 11:46:00 +0530

    Deleted extra files

--- dashboard/src/Components/Hamburger.js
+++
@@ -1,9 +0,0 @@
-import HamIcon from "../Assets/Hamburger_icon.svg.png";
-
-export default function Hamburger({ hamStyle, onHamClick }) {
-  return (
-    <div onClick={onHamClick}>
-      <img src={HamIcon} style={hamStyle}/>
-    </div>
-  );
-}
